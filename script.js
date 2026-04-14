8acb4c7f79ea76c541c551eca452dc38e63fc43e
Hirdyansh Bhadauria <hirdyanshbhadauria2908@gmail.com>
2026-04-14 15:59:01 +0530

    Added login feature

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Student Management System Loaded");
+
+function login(username, password) {
+    if (username === "admin" && password === "1234") {
+        console.log("Login successful");
+    } else {
+        console.log("Invalid credentials");
+    }
+}
